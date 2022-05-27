715a467da79618b4191f93b20df2a9d81f1cb76c
Phil <phil.trumpy@gmail.com>
2022-05-26 20:39:32 -0500

    added update function to database.py

--- database.py
+++ database.py
@@ -9,12 +9,9 @@ def add_record(upc, series, year, description, quantity, notes):
     cur.execute(
         sql.SQL("insert into {} VALUES (%s, %s, %s, %s, %s, %s)")
             .format(sql.Identifier('ornaments')),
-        [upc,series,year,description, quantity, notes]
-        )
-
+        [upc,series,year,description, quantity, notes])
 
     con.commit()
-
     pool.putconn(con)
 
 def initial_data():
@@ -33,5 +30,15 @@ def delete_records(upc):
     query = sql.SQL ("delete from {table} where {field} = %s").format(table=sql.Identifier('ornaments'),
     field=sql.Identifier('upc_code'))
     cur.execute(query,(upc,))
+    con.commit()
+    pool.putconn(con)
+
+#update records
+def update_records(series, year, desc, qty, notes):
+    con = pool.getconn()
+    cur = con.cursor()
+    query = sql.SQL("update {table} set {field1} = %s, {field2} = %s,{field3} = %s"
+    ",{field4}= %s,{field5}=%s where {field6} = %s").format(table=sql.Identifier('ornaments'),
+    field1=sql.Identifier('series'),field2=sql.Identifier('year'))
     con.commit()
     pool.putconn(con)
